82f44e81a69b21ccb86a93001ae07c8bc80b3e5c
rajderks <2419711+rajderks@users.noreply.github.com>
2021-06-14 16:47:29 +0200

    Add sync function

--- src/routes/mapSyncRouter.ts
+++ src/routes/mapSyncRouter.ts
@@ -1,32 +1,56 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import MapModel, { MapAttr } from '../models/Map';
-import {
-  objectReduceMissingKeys,
-  genericAPIError,
-  mapPath,
-  mapWrite,
-  mapDelete,
-} from '../utils';
-import sequelize from '../sequelize';
-import log from '../log';
-import multer from 'multer';
-import fs from 'fs';
-import path from 'path';
-import { File } from '../types';
-import { uuid } from 'uuidv4';
-import { path as rootPath } from 'app-root-path';
-import { sync as rimrafsync } from 'rimraf';
+import { Op } from 'sequelize';
+import { genericAPIError } from '../utils';
 
 const MapSyncRouter = express.Router();
-const upload = multer();
 
 // for parsing application/json
 MapSyncRouter.use(bodyParser.json());
 
 MapSyncRouter.post('/', async (req, res) => {
-  const bodyParams: Partial<MapAttr> = req.body;
-  MapModel.findAll({ attributes: ['name', 'version'] });
+  const bodyParams: Partial<MapAttr>[] = req.body;
+  if (!Array.isArray(bodyParams)) {
+    return genericAPIError(
+      res,
+      400,
+      `Params is not of type Array ${typeof bodyParams}`
+    );
+  }
+  const mapNames = bodyParams
+    .map((m) => (m.identifier?.length ? m.identifier : null))
+    .filter((m) => !!m);
+  const mapsAndVersions = await MapModel.findAll({
+    attributes: ['identifier', 'version'],
+    where: {
+      identifier: {
+        [Op.in]: mapNames,
+      },
+    },
+  });
+  const mapsAndVersionsJSON: {
+    identifier: string;
+    version: string;
+  }[] = mapsAndVersions.map(
+    (model) => model.toJSON() as { identifier: string; version: string }
+  );
+  if (!Array.isArray(mapsAndVersionsJSON) || !mapsAndVersionsJSON?.length) {
+    return genericAPIError(res, 404, 'Maps not found');
+  }
+  const mismatchedMaps: Record<string, string> = {};
+  bodyParams.forEach((value) => {
+    const map = mapsAndVersionsJSON.find(
+      (m) => m.identifier === value.identifier
+    );
+    if (!map) {
+      return;
+    }
+    if (map.version !== value.version) {
+      mismatchedMaps[value.identifier] = value.version;
+    }
+  });
+  res.json(mismatchedMaps);
 });
 
 export default MapSyncRouter;
